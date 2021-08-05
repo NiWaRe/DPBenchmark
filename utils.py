cb18cba8d5605eace26ac5132588fa0c640d1f51
NiWaRe <n.remerscheid@bluewin.ch>
2021-08-05 15:25:53 +0000

    add preliminary solution to be able to pass in the ModelSurgeon from configs

---
+++ utils.py
@@ -0,0 +1,28 @@
+from deepee import SurgicalProcedures
+from torch import nn, Size
+from typing import Union
+
+# TODO: think of more elegant way of making them available natively
+
+# to make surgical procedures available as part of a module,
+# so that they can be passed to Callable types from the configs.yaml
+
+def BN_to_IN(module: nn.modules.batchnorm._BatchNorm) -> nn.Module:
+    return SurgicalProcedures.BN_to_IN(module)
+
+def BN_to_BN_nostats(module: nn.modules.batchnorm._BatchNorm) -> nn.Module:
+    return SurgicalProcedures.BN_to_BN_nostats(module)
+
+def IN_to_IN_nostats(module: nn.modules.instancenorm._InstanceNorm) -> nn.Module:
+    return SurgicalProcedures.IN_to_IN_nostats(module)
+
+def BN_to_GN(
+    module: nn.modules.batchnorm._BatchNorm, num_groups: Union[str, int] = "default"
+) -> nn.Module:
+    return SurgicalProcedures.BN_to_GN(module, num_groups)
+
+def BN_to_LN(
+    module: nn.Module,
+    normalized_shape: Union[int, list, Size],
+) -> nn.Module:
+    return SurgicalProcedures.BN_to_LN(module, normalized_shape)
